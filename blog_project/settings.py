ad112c731750ca26c168c16b6a1fd908c04fbf5c
Sobirov Jamshid <75526925+MrDeverloper@users.noreply.github.com>
2021-04-12 10:39:28 +0500

    Update settings.py

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'b=0u+6=qv070c12&^g#h^nf$0874od)x6x!c))^09$_58&s^ve'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['portfoilojss.heroku.com', '127.0.0.1]
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
